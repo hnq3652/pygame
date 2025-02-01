c73ea04c1a1a15c4a6223b067cce58a454dce676
tea-tya <yamanaka6020@gmail.com>
2025-02-01 12:02:26 +0900

    M3-2まで
    WASDでプレイヤーの移動、絵の表示、移動範囲の制限

--- tutorial.py
+++ tutorial.py
@@ -1,12 +1,17 @@
 import pygame as pg  #pygameモジュールをインポートする
 from pygame.locals import*  #pygameモジュールの定数をインポートする(event.type: QUITなど)
 
+SCREEN_WIDTH = 800  #画面の横幅を定数にする
+SCREEN_HEIGHT = 600  #画面の縦幅
+PLAYER_SPEED = 5  #プレイヤーのスピード
+FPS = 30  #フレームレート
+
 def __init__():  #ゲームの初期設定をする関数を定義する
-    global screen, player_color, player  #画面、プレイヤーの色、プレイヤーをグローバル化
+    global screen, player_image, player  #画面、プレイヤーの画像、プレイヤーをグローバル化
     pg.init()  #pygameを初期化?実行?する
-    screen = pg.display.set_mode((800, 600))  #画面サイズを横800、縦600にする
-    player_color = (0, 255, 255)  #プレイヤーの色を水色(カラーコード:00ffff)にする
-    player = Rect((0, 200), (60, 60))  #プレイヤーを座標(x,y)=(0,200)、(横,縦)=(60,60)の長方形にする
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  #画面サイズを横800、縦600にする
+    player_image = pg.transform.smoothscale(pg.image.load("images/robonyan.png").convert_alpha(), (60, 60))  #プレイヤーの画像を読み込んで(60,60)の大きさに調整する
+    player = player_image.get_rect(x=0, y=200)  #プレイヤーを位置(0,200)の長方形にする
     while True:  #ずっと繰り返す
         update()  #プレイヤーの座標など、動きの更新の関数を実行する
         draw()  #描画の関数を実行する
@@ -14,16 +19,22 @@ def __init__():  #ゲームの初期設定をする関数を定義する
         for event in pg.event.get():  #イベント処理(ゲーム(プログラム)の終了、フルスクリーン切り替えなど)
             if event.type == QUIT or pg.key.get_pressed()[K_ESCAPE]:  #画面の×ボタンが押されるか、escapeキーが押されたら
                 return  #プログラムを終了する(ゲーム終了)
-        pg.time.Clock().tick(30)  #30FPSで動くように調整する
+        pg.time.Clock().tick(FPS)  #30FPSで動くように調整する
 
 def update():  #動き(プレイヤーの座標など)の更新をする関数を定義する
     global player  #プレイヤーをグローバル化
-    player.x = player.x + 5  #プレイヤーのx座標を5増やす(右に5動く)
-    if player.x > 800:  #もしx座標が画面の横幅より大きいなら(画面右側で見切れたら)
-        player.x = 0  #x座標を0にする
+    if pg.key.get_pressed()[K_w]:  #もしWが押されたら(長押し可能)
+        player.y = player.y - PLAYER_SPEED  #プレイヤーのy座標を5減らす(上に5動く)
+    elif pg.key.get_pressed()[K_s]:
+        player.y = player.y + PLAYER_SPEED  #プレイヤーのy座標を5増やす(下に5動く)
+    if pg.key.get_pressed()[K_a]:
+        player.x = player.x - PLAYER_SPEED  #プレイヤーのx座標を5減らす(左に5動く)
+    elif pg.key.get_pressed()[K_d]:
+        player.x = player.x + PLAYER_SPEED  #プレイヤーのx座標を5増やす(右に5動く)
+    player.clamp_ip((0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT))  #プレイヤーが(0,0)から(800,600)の範囲から出ないようにする
 
 def draw():  #描画をする関数を定義する
     screen.fill((0, 0, 0))  #画面を黒色(カラーコード:000000)で塗りつぶす
-    pg.draw.rect(screen, player_color, player)  #screenに、水色の、プレイヤーを配置する
+    screen.blit(player_image, player)  #player_imageをplayerの位置に配置する
 
 __init__()  #プログラムを実行する
--- tutorial_no_comment.py
+++ tutorial_no_comment.py
@@ -1,12 +1,17 @@
 import pygame as pg
 from pygame.locals import*
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+PLAYER_SPEED = 5
+FPS = 30
+
 def __init__():
-    global screen, player_color, player
+    global screen, player_image, player
     pg.init()
-    screen = pg.display.set_mode((800, 600))
-    player_color = (0, 255, 255)
-    player = Rect((0, 200), (60, 60))
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_image = pg.transform.smoothscale(pg.image.load("images/robonyan.png").convert_alpha(), (60, 60))
+    player = player_image.get_rect(x=0, y=200)
     while True:
         update()
         draw()
@@ -14,16 +19,22 @@ def __init__():
         for event in pg.event.get():
             if event.type == QUIT or pg.key.get_pressed()[K_ESCAPE]:
                 return
-        pg.time.Clock().tick(30)
+        pg.time.Clock().tick(FPS)
 
 def update():
     global player
-    player.x = player.x + 5
-    if player.x > 800:
-        player.x = 0
+    if pg.key.get_pressed()[K_w]:
+        player.y = player.y - PLAYER_SPEED
+    elif pg.key.get_pressed()[K_s]:
+        player.y = player.y + PLAYER_SPEED
+    if pg.key.get_pressed()[K_a]:
+        player.x = player.x - PLAYER_SPEED
+    elif pg.key.get_pressed()[K_d]:
+        player.x = player.x + PLAYER_SPEED
+    player.clamp_ip((0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def draw():
     screen.fill((0, 0, 0))
-    pg.draw.rect(screen, player_color, player)
+    screen.blit(player_image, player)
 
 __init__()
